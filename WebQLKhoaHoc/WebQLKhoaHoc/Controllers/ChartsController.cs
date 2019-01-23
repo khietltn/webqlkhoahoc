3d6b73758d2ce29d7a62aed4fc0d2391ed406020
Sang <sanglun2254@gmail.com>
2019-01-23 14:36:00 +0700

    Thống kê Đội ngũ KHCN

---
+++ WebQLKhoaHoc/WebQLKhoaHoc/Controllers/ChartsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebQLKhoaHoc.Controllers
+{
+    public class ChartsController : Controller
+    {
+        private QLKhoaHocEntities db = new QLKhoaHocEntities();
+        // GET: Charts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThongKe(string unit)
+        {
+            if(unit == "total")
+            {
+                return RedirectToAction("totalCharts");
+            }
+            else if(unit == "degree")
+            {
+                return RedirectToAction("degreeCharts");
+            }
+            else
+            {
+                return RedirectToAction("quantumCharts");
+            }
+        }
+
+        public ActionResult totalCharts()
+        {
+            return View();
+        }
+
+        public ActionResult degreeCharts()
+        {
+            return View();
+        }
+        public ActionResult quantumCharts()
+        {
+            return View();
+        }
+    }
+}
