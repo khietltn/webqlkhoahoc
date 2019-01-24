7c9400937eb42a09c441dfb5a756c72cb4ed56c3
Sang <sanglun2254@gmail.com>
2019-01-24 23:44:53 +0700

    update chart topic

---
+++ WebQLKhoaHoc/WebQLKhoaHoc/Models/topicdata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebQLKhoaHoc.Models
+{
+    public class topicdata
+    {
+        public int MaDVQL { get; set; }
+        public DateTime fromdate { get; set; }
+        public DateTime todate { get; set; }
+
+        public static topicdata Mapping(int madvql,DateTime fd,DateTime td)
+        {
+            topicdata topicdata = new topicdata();
+            topicdata.MaDVQL = madvql;
+            topicdata.fromdate = fd;
+            topicdata.todate = td;
+
+            return topicdata;
+        }
+    }
+}
