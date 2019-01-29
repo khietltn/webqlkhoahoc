6171221b0e61440019e2df445236ff136b134045
Hieu-PC <hieu.tran@watasolutions.com>
2019-01-28 23:57:44 -0800

    Add web.config  Prod

--- WebQLKhoaHoc/WebQLKhoaHoc/QLKH.Designer.cs
+++ WebQLKhoaHoc/WebQLKhoaHoc/QLKH.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\home\Documents\webqlkhoahoc\WebQLKhoaHoc\WebQLKhoaHoc\QLKH.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\webqlkhoahoc\WebQLKhoaHoc\WebQLKhoaHoc\QLKH.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
