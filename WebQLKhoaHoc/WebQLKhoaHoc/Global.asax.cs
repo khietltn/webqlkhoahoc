a9ae8554e758e12eb1ccef7811fab8e56374389f
Hieu-PC <hieu.tran@watasolutions.com>
2019-02-16 00:26:55 +0700

    Impplement HomePage UI

--- WebQLKhoaHoc/WebQLKhoaHoc/Global.asax.cs
+++ WebQLKhoaHoc/WebQLKhoaHoc/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +17,27 @@ namespace WebQLKhoaHoc
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            if (!File.Exists(Server.MapPath("Count_Visited.txt")))
+            File.WriteAllText(Server.MapPath("Count_Visited.txt"),"0");
+            Application["DaTruyCap"] = int.Parse(File.ReadAllText(Server.MapPath("Count_Visited.txt")));
+        }
+        void Session_Start(object sender, EventArgs e)
+        {
+            // Tăng số đang truy cập lên 1 nếu có khách truy cập
+            if (Application["DangTruyCap"] == null)
+                Application["DangTruyCap"] = 1;
+            else
+                Application["DangTruyCap"] = (int)Application["DangTruyCap"] + 1;
+            // Tăng số đã truy cập lên 1 nếu có khách truy cập
+            Application["DaTruyCap"] = (int)Application["DaTruyCap"] + 1;
+            File.WriteAllText(Server.MapPath("Count_Visited.txt"), Application["DaTruyCap"].ToString());
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            //Khi hết session hoặc người dùng thoát khỏi website thì giảm số người đang truy cập đi 1
+            Application["DangTruyCap"] = (int)Application["DangTruyCap"] - 1;
         }
     }
 }
